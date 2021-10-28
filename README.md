20fb9bd007e49906faadd1a180b10ca66041a7fd
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2021-10-27 23:07:51 -0500

    Try to simplify and let the project guide the structure

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rompecabezas
+
+Esta es una aplicación de rompecabezas. Hace parte de mi trabajo de grado que es
+un estudio de los métodos de percolación aplicados al armar un rompecabezas.
+
+Iré profundizando más en esto.
--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,73 @@ import "@interactjs/actions/drag";
 import "@interactjs/actions/drop";
 import "@interactjs/dev-tools";
 import interact from "@interactjs/interact";
-import jigsaw from "./components/jigsaw";
+import Reef from "reefjs";
+
+//
+// Variables
+//
+let jigsaw = new Reef("#jigsaw", {
+    data: {
+        placed: [],
+    },
+    template,
+});
+
+//
+// Methods
+//
+
+function template(props) {
+    let numPieces = 16;
+    let placedContainer = [];
+
+    for (let i = 0; i < numPieces; i++) {
+        placedContainer.push(props.placed.includes(i) ? 1 : 0);
+    }
+
+    return `
+        <div class="jigsaw">
+            <div class="container solution">
+                ${placedContainer
+                    .map(function (piece, index) {
+                        return `<div class="piece ${
+                            piece === 1 ? "placed" : ""
+                        }" data-dropzone data-id="${index}">${index}</div>`;
+                    })
+                    .join("")}
+            </div>
+            <div class="container pieces">
+                ${placedContainer
+                    .map(function (piece, index) {
+                        let content = "";
+                        if (piece === 0) {
+                            content = `<div class="piece" data-draggable id=${index}>${index}</div>`;
+                        }
+                        return content;
+                    })
+                    .join("")}
+            </div>
+        </div>
+    `;
+}
+
+function dragMoveListener(event) {
+    let target = event.target;
+    // keep the dragged position in the data-x/data-y attributes
+    let x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
+    let y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+
+    // translate the element
+    target.style.transform = "translate(" + x + "px, " + y + "px)";
+
+    // update the posiion attributes
+    target.setAttribute("data-x", x);
+    target.setAttribute("data-y", y);
+}
+
+//
+// Inits & Event Listeners
+//
 
 jigsaw.render();
 
@@ -38,17 +104,3 @@ interact("[data-dropzone]").dropzone({
         }
     },
 });
-
-function dragMoveListener(event) {
-    var target = event.target;
-    // keep the dragged position in the data-x/data-y attributes
-    var x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
-    var y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
-
-    // translate the element
-    target.style.transform = "translate(" + x + "px, " + y + "px)";
-
-    // update the posiion attributes
-    target.setAttribute("data-x", x);
-    target.setAttribute("data-y", y);
-}
