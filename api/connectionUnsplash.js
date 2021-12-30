4171d207b521d3b287a41bdb2a5f45349c384036
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2021-12-30 00:20:29 -0500

    :sparkles: Add middleman that fetches from unsplash

---
+++ api/connectionUnsplash.js
@@ -0,0 +1,26 @@
+const fetch = (...args) =>
+    import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+const unsplashAccessKey = process.env.UNSPLASH_ACCESS_KEY;
+
+exports.handler = async function (event, context) {
+    try {
+        const response = await fetch(
+            `https://api.unsplash.com/photos/random?client_id=${unsplashAccessKey}&count=10`
+        );
+
+        if (!response.ok) throw new Error(response);
+
+        const data = await response.json();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: error.toString(),
+        };
+    }
+};
--- src/js/unsplash/index.js
+++ src/js/unsplash/index.js
@@ -2,5 +2,22 @@
 // Functions
 //
 function getRandomPictures() {
-    fetch("Random picture from unsplash through middleman api");
+    fetch("/.netlify/functions/connectionUnsplash")
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response);
+        })
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 }
+
+//
+// Inits
+//
+getRandomPictures();
